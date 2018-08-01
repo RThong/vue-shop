92d2ab6a3024aabfc05b9d094a974e7c12ace975
RThong <403216075@qq.com>
2018-08-01 17:04:00 +0800

    8.1

---
+++ test/a.vue
@@ -0,0 +1,36 @@
+<template>
+	<div>a
+		<router-link to="/b">click me</router-link>
+	</div>
+</template>
+<script>
+	export default {
+		data() {
+			return {
+				i: 0,
+				timer: ''
+			}
+		},
+		mounted() {
+			console.log(this.i);
+			this.timer = setInterval(() => {
+				console.log(this.i++);
+			}, 1000)
+			this.$once('hook:beforeDestroy', () => {
+				clearInterval(this.timer)
+			})
+		},
+		// activated() {
+		// 	console.log(this.i);
+		// 	this.timer = setInterval(() => {
+		// 		console.log(this.i++);
+		// 	}, 1000)
+		// },
+		// deactivated() {
+		// 	clearInterval(this.timer)
+		// }
+		// beforeDestroy() {
+		// 	clearInterval(this.timer)
+		// }
+	}
+</script>
